49ac718b12b5bc9e76a5dee5d6276800d30e3c3c
alefra88 <efra.rlz@gmail.com>
2023-03-07 07:54:12 -0600

    keys xd

--- stripe-keys.js
+++ stripe-keys.js
@@ -1,6 +1,6 @@
 export default {
   public:
-    "pk_test_51Miy3HDXOT2uujtpXX5ayAy290Wqd070HAbyFfCh6EsdxllsiQPofwt3wqniumVzDmPj0zReApGpK0yI6i4koxrq00WpnH6fU2",
+    "",
   secret:
-    "sk_test_51Miy3HDXOT2uujtpavqkd4KpAhPybVgZj6y3um1FjnHbZwpiBlYZfwyPuBqREvjyQyilMRwvlUVHH2TaZP5Z9C2t00DdEEr49c",
+    "",
 };
